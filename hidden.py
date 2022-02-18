3b792cfabcdedeaecfc8f3bab55ab81cb70fa8e1
smorya <maryana.skoropad@gmail.com>
2022-02-18 22:46:25 +0200

    final

--- hidden.py
+++ hidden.py
@@ -4,7 +4,7 @@
 # Create new App and get the four strings
 
 def oauth():
-    return {"consumer_key": "0PlKlKMbNYWL9UeVuC6DPxX4B",
-            "consumer_secret": "dpvpCGjA9udpjsPkCVfdxZhmAOLMXFeZcZEVLbH3usRiv8fHfN",
-            "token_key": "963011359737774080-mYiiiIGOarKbuYGVdVTJBPcYC39RkqC",
-            "token_secret": "f8031GVQo6dJXX35RRVbDlGxMcitrAt7uyAUVgKuO1E0R"}
+    return {"consumer_key": "pass the key",
+            "consumer_secret": "pass the key",
+            "token_key": "pass the key",
+            "token_secret": "pass the key"}
